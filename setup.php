ae346cbae35c8d9fe87c3effe2ea98ab53234ff0
Chung-li (aliku) Hsiao <aliku@stirprefer.corp.sg3.yahoo.com>
2013-08-15 10:34:32 +0000

    add server side code to store data in apc

--- setup.php
+++ setup.php
@@ -1,3 +1,8 @@
 <?php
-echo time();
-error_log(print_r($_POST,true));
+//echo time();
+error_log(__FILE__.print_r($_POST,true));
+$data = $_POST['data'];
+$user = $_POST['user'];
+$dataStr = implode($data);
+$dataMd5 = md5($dataStr);
+apc_add($dataMd5, $user);
--- verify.php
+++ verify.php
@@ -1,3 +1,14 @@
 <?php
-echo time();
-error_log(print_r($_POST,true));
+//echo time();
+error_log(__FILE__.print_r($_POST,true));
+$data = $_POST['data'];
+$dataStr = implode($data);
+$dataMd5 = md5($dataStr);
+$result = apc_fetch($dataMd5);
+
+if(!$result) {
+	//handle error
+    echo "Invalid User";
+} else {
+    echo $result;
+}
