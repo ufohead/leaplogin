85122a839e24a18ce0d1328a8542fd71ae737c0b
Aliku Hsiao <aliku@hishonorpull-lm.tpcity.corp.yahoo.com>
2013-08-16 10:58:06 +0800

    output to page instead of alert add duplicated check in setup server code

--- setup.php
+++ setup.php
@@ -1,8 +1,26 @@
 <?php
 //echo time();
 error_log(__FILE__.print_r($_POST,true));
-$data = $_POST['data'];
+
 $user = $_POST['user'];
+$existUser = apc_fetch($user);
+
+// check existed user
+if($existUser !== false) {
+	$oldDataMd5 = $existUser;
+	apc_delete($user);
+	apc_delete($oldDataMd5);
+}
+
+$data = $_POST['data'];
 $dataStr = implode($data);
 $dataMd5 = md5($dataStr);
-apc_add($dataMd5, $user);
+
+$existData = apc_fetch($dataMd5);
+if ($existData !== false) {
+	echo 'Setup fail, duplicated setting with others';
+} else {
+	apc_add($dataMd5, $user);
+	apc_add($user, $dataMd5);
+	echo 'Setup OK, user: '.$user;	
+}
